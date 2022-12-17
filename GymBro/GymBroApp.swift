5f51c9e697cdcc089cea4e3c84f4fc056054e21d
Bartosz Rzechółka <27322@s.am.szczecin.pl>
2022-12-17 17:45:12 +0100

    Initial Commit

---
+++ GymBro/GymBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBroApp.swift
+//  GymBro
+//
+//  Created by Bartosz Rzechółka on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GymBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
